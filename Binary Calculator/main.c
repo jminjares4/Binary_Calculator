0ad4f0d825954c50fb61c2edc028a056d759e9b5
Jesus Minjares <60948298+jminjares4@users.noreply.github.com>
2021-03-01 16:37:24 -0700

    Update main.c

--- Binary Calculator/main.c
+++ Binary Calculator/main.c
@@ -26,26 +26,26 @@ void main(void){
 	uint8_t operand = 0, number1 = 0, number2 = 0, enter = 0;
 
 	while(1){
-	    wait(); //wait for enter to be pressed
-	    //read BCD, store in number and display in LCD
-	    number1  = read_bcd(); lcdSetInt(number1, 0,0);
-	    wait();
-	    //read operand, store in operand and display in LCD
-	    operand = read_operand(); lcdSetText((char*)get_operand(operand), 2,0);
-	    wait();
-	    //read BCD for number 2
-        number2  = read_bcd(); lcdSetInt(number2,3,0);
-        //read enter
-        enter = read_enter();
-        if(enter){
-           lcdSetText("enter",8,0); //display enter
-           display(number1, number2, operand); //display output to LCD
-        }
-        else{
-            wait();
-            lcdSetText("enter",8,0); //display enter
-            display(number1, number2, operand); //display output to LCD
-        }
-        number1 = number2 = operand = enter = 0; //set all varaibles to 0
+		wait(); //wait for enter to be pressed
+		//read BCD, store in number and display in LCD
+		number1  = read_bcd(); lcdSetInt(number1, 0,0);
+		wait();
+		//read operand, store in operand and display in LCD
+		operand = read_operand(); lcdSetText((char*)get_operand(operand), 2,0);
+		wait();
+		//read BCD for number 2
+		number2  = read_bcd(); lcdSetInt(number2,3,0);
+		//read enter
+		enter = read_enter();
+		if(enter){
+			lcdSetText("enter",8,0); //display enter
+			display(number1, number2, operand); //display output to LCD
+		}
+		else{
+			wait();
+			lcdSetText("enter",8,0); //display enter
+			display(number1, number2, operand); //display output to LCD
+		}
+		number1 = number2 = operand = enter = 0; //set all varaibles to 0
 	}
 }
